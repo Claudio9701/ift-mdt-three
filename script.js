88bd3a186100c10113ef9d2b77b2aea3df8a81d1
Claudio <claudio.rtega2701@gmail.com>
2024-01-12 17:50:32 +0100

    change object add interactivity

--- script.js
+++ script.js
@@ -1,15 +1,66 @@
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 import { LumaSplatsThree, LumaSplatsSemantics } from 'luma-web';
 
 
-let camera, scene, renderer;
+let camera, scene, renderer, model;
 
-init();
+const raycaster = new THREE.Raycaster();
+let pointerPosition = { x: 0, y: 0 };
+window.addEventListener('click', (event) => {
 
-function init() {
+    pointerPosition.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointerPosition.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    console.log('click', pointerPosition)
+});
+
+let keyPressed;
+window.addEventListener('keydown', (event) => {
+    console.log('keydown', event.key)
+    keyPressed = event.key;
+});
+
+// Add a dropdown menu to select the luma source
+let select = document.createElement('select');
+select.id = 'source';
+
+// Options
+let options = [
+    'La Defense Statue',
+    'IFT 6th floor space',
+    'IFT Staff Room'
+];
+let values = [
+    'https://lumalabs.ai/capture/ddc47b58-fe0e-4c99-ba45-f9ab2a1bf5f6',
+    'https://lumalabs.ai/capture/97352f55-c5f2-42d4-a867-ed676acfd90b',
+    'https://lumalabs.ai/capture/251afeaa-6802-4017-8ead-6df9b3fdc190'
+];
+
+for (let i = 0; i < options.length; i++) {
+    let option = document.createElement('option');
+    option.value = values[i];
+    option.text = options[i];
+    select.appendChild(option);
+}
+
+select.onchange = () => {
+    // remove scene
+    scene.remove(scene.children[0]);
+    // free memory
+    renderer.dispose();
+    init(select.value);
+    animate();
+};
+document.body.appendChild(select);
+
+select.value = values[0];
+init(select.value);
+
+function init(source) {
 
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
     camera.position.set(- 1.8, 0.6, 2.7);
@@ -19,19 +70,50 @@ function init() {
     let uniformTime = new THREE.Uniform(0);
 
     let splats = new LumaSplatsThree({
-        source: 'https://lumalabs.ai/capture/251afeaa-6802-4017-8ead-6df9b3fdc190',
-
+        // La Defense Statue
+        source: source,
         // disable loading animation so model is fully rendered after onLoad
-        loadingAnimationEnabled: false,
+        loadingAnimationEnabled: true,
         onBeforeRender: () => {
             uniformTime.value = performance.now() / 100;
         }
     });
 
+    splats.onLoad = () => {
+        splats.captureCubemap(renderer).then(capturedTexture => {
+            scene.environment = capturedTexture;
+            scene.background = capturedTexture;
+            scene.backgroundBlurriness = 0.5;
+        });
+    }
+
     scene.add(splats);
 
     // filter splats to only show foreground layers
-    splats.semanticsMask = LumaSplatsSemantics.BACKGROUND;
+    splats.semanticsMask = LumaSplatsSemantics.FOREGROUND | LumaSplatsSemantics.BACKGROUND;
+
+    // move camera to ideal viewing position
+    splats.onInitialCameraTransform = transform => {
+        camera.matrix.copy(transform);
+        camera.matrix.decompose(camera.position, camera.quaternion, camera.scale);
+        camera.position.y = 0.25;
+    };
+
+    // load glb
+    let loader = new GLTFLoader();
+    loader.load('models/gltf/a2301035b201_model.glb', (gltf) => {
+        model = gltf.scene;
+        model.position.y = 0.5;
+        model.position.x = 0;
+        model.position.z = -2;
+        // rotate
+        model.rotation.y = Math.PI / 2;
+
+        scene.add(model);
+    });
+
+    scene.fog = new THREE.FogExp2(new THREE.Color(0xe0e1ff).convertLinearToSRGB(), 0.15);
+    scene.background = scene.fog.color;
 
     splats.setShaderHooks({
         vertexShaderHooks: {
@@ -56,9 +138,6 @@ function init() {
         }
     });
 
-    scene.fog = new THREE.FogExp2(new THREE.Color(0xe0e1ff).convertLinearToSRGB(), 0.15);
-    scene.background = scene.fog.color;
-
     const canvas = document.getElementById('canvas');
     // Create a WebGLRenderer and set its width and height
     renderer = new THREE.WebGLRenderer({
@@ -73,9 +152,9 @@ function init() {
 
 
     const controls = new OrbitControls(camera, canvas);
-    controls.addEventListener('change', render); // use if there is no animation loop
+    // controls.addEventListener('change', render); // use if there is no animation loop
     controls.minDistance = 2;
-    controls.maxDistance = 10;
+    controls.maxDistance = 100;
     controls.target.set(0, 0, - 0.2);
     controls.update();
 
@@ -87,6 +166,7 @@ function init() {
 
     document.body.appendChild(vrButton);
 
+    render();
 }
 
 function onWindowResize() {
@@ -100,10 +180,110 @@ function onWindowResize() {
 
 }
 
-//
-
 function render() {
 
     renderer.render(scene, camera);
+    animate();
 
 }
+
+function controlObjectKeys() {
+    if (!keyPressed) return;
+    // Move the object forward
+    if (keyPressed === "W") {
+        model.position.z -= 0.1;
+        keyPressed = null;
+    }
+    // Move the object backward
+    if (keyPressed === "S") {
+        model.position.z += 0.1;
+        keyPressed = null;
+    }
+    // Move the object to the left
+    if (keyPressed === "A") {
+        model.position.x -= 0.1;
+        keyPressed = null;
+    }
+    // Move the object to the right
+    if (keyPressed === "D") {
+        model.position.x += 0.1;
+        keyPressed = null;
+    }
+    // Move the object up
+    if (keyPressed === "E") {
+        model.position.y += 0.1;
+        keyPressed = null;
+    }
+    // Move the object down
+    if (keyPressed === "Q") {
+        model.position.y -= 0.1;
+        keyPressed = null;
+    }
+
+    // Rotate the object to the left
+    if (keyPressed === "K") {
+        model.rotation.y -= 0.1;
+        keyPressed = null;
+    }
+    // Rotate the object to the right
+    if (keyPressed === "L") {
+        model.rotation.y += 0.1;
+        keyPressed = null;
+    }
+    // Rotate the object up
+    if (keyPressed === "N") {
+        model.rotation.x -= 0.1;
+        keyPressed = null;
+    }
+    // Rotate the object down
+    if (keyPressed === "M") {
+        model.rotation.x += 0.1;
+        keyPressed = null;
+    }
+
+    // Scale the object up
+    if (keyPressed === "Z") {
+        model.scale.x += 0.1;
+        model.scale.y += 0.1;
+        model.scale.z += 0.1;
+        keyPressed = null;
+    }
+    // Scale the object down
+    if (keyPressed === "X") {
+        model.scale.x -= 0.1;
+        model.scale.y -= 0.1;
+        model.scale.z -= 0.1;
+        keyPressed = null;
+    }
+
+}
+
+function animate() {
+
+    renderer.setAnimationLoop(render);
+
+    controlObjectKeys();
+
+    // update the picking ray with the camera and mouse position
+    raycaster.setFromCamera(pointerPosition, camera);
+
+
+
+    if (model) {
+        // calculate objects intersecting the picking ray
+        const intersects = raycaster.intersectObjects(scene.children, true);
+        model.traverse((child) => {
+            if (child.isMesh) {
+                if (intersects.length > 0) {
+                    console.log('intersects', intersects)
+                    child.material.color.set(0xff0000);
+                } else {
+                    console.log('no intersects')
+                    child.material.color.set(0xffffff);
+                }
+            }
+        });
+    };
+}
+
+
